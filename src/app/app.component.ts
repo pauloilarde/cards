f2c427120db143fd2c4e2084d7f27b64964d62d1
Paulo Ilarde <pauloilarde@gmail.com>
2020-01-22 12:45:03 -0300

    Melhorando os componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      userName: 'nature',
+      content: 'I saw this neat tree today.'
+    },
+    {
+      title: 'Snowy mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      userName: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain.'
+    },
+    {
+      title: 'Mountain bike',
+      imageUrl: 'assets/biking.jpeg',
+      userName: 'radicalbiking',
+      content: 'I did some biking today.'
+    }
+  ]
 }
